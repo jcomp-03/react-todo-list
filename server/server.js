d6c53442e3f6098028aa40f1e3c146347856f4b8
jcomp-03@James-Desktop <jcomp_03@yahoo.com>
2022-11-22 23:43:48 -0500

    Created server folder and created server.js. Continuing buildout of backend.

---
+++ server/server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+
+// Serve up static assets; only runs in production environment
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost:27017/pizza-hunt",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+// Use this to log mongo queries being executed!
+mongoose.set("debug", true);
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
+// });
+
+app.use(require("./routes"));
+
+app.listen(PORT, () =>
+  console.log(`-----🌍 Connected on localhost:${PORT}-----`)
+);
