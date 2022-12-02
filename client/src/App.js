54c2df39b96e02fd7fc9da8f9cc40ec8c99c35f6
jcomp-03@James-Desktop <jcomp_03@yahoo.com>
2022-12-01 20:35:07 -0500

    Removed initial state for todoList in App.js. Page loads without any todos now.

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import { useState } from "react";
 import Header from "./components/Header";
 import ToDoForm from "./components/Form";
 import ToDoBoard from "./components/ToDoBoard";
-import { nanoid } from "nanoid";
 
 function App() {
   // state variable for showing/hiding todo board
@@ -10,51 +9,7 @@ function App() {
 
   // state for todo list
   const [todoList, setTodoList] = useState(
-    new Map([
-      [
-        "2022-11-29",
-        [
-          {
-            taskDate: "2022-11-29",
-            taskDescription: "lorem",
-            taskId: `todo-${nanoid()}`,
-            taskTags: ["for-other", "work"],
-            taskTitle: "Title 4",
-          },
-        ],
-      ],
-      [
-        "2022-11-28",
-        [
-          {
-            taskDate: "2022-11-28",
-            taskDescription: "lorem ipsum",
-            taskId: `todo-${nanoid()}`,
-            taskTags: ["for-other", "work"],
-            taskTitle: "Title 3",
-          },
-        ],
-      ],
-      [
-        "2022-11-25",
-        [
-          {
-            taskDate: "2022-11-25",
-            taskDescription: "lorem ipsum ipsum",
-            taskId: `todo-${nanoid()}`,
-            taskTags: ["work"],
-            taskTitle: "Title 1",
-          },
-          {
-            taskDate: "2022-11-25",
-            taskDescription: "lorem lorem",
-            taskId: `todo-${nanoid()}`,
-            taskTags: ["for-other"],
-            taskTitle: "Title 2",
-          },
-        ],
-      ],
-    ])
+    new Map([])
   );
 
   return (
